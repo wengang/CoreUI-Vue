c3c5e733aa59e80dd13a3b6d44d83588f11aac54
Yuwg <tao-yu@hotmail.com>
2017-11-22 18:20:15 +0800

    integration backend api

--- Vue_Starter/src/router/index.js
+++ Vue_Starter/src/router/index.js
@@ -27,6 +27,31 @@ export default new Router({
         }
 
       ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/views/Login.vue')
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: () => import('@/views/Register.vue')
+    },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: () => import('@/views/Page404.vue')
+    },
+    {
+      path: '/500',
+      name: 'InternalError',
+      component: () => import('@/views/Page500.vue')
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: () => import('@/views/Page404.vue')
     }
   ]
 })
--- Vue_Starter/src/views/Dashboard.vue
+++ Vue_Starter/src/views/Dashboard.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="animated fadeIn">
-    Hello World
+    <ul id="example-1">
+      <li v-for="item in users">
+        {{ item.name }}
+      </li>
+</ul>
   </div>
 </template>
 
 <script>
+import Axios from 'axios'
+var data = { users: [] }
 export default {
-  name: 'dashboard'
+  name: 'dashboard',
+  data () {
+    return data
+  },
+  mounted () {
+    Axios.get('/api/hello/users')
+      .then(response => {
+        data.users = response.data.content
+      })
+  }
 }
 </script>
